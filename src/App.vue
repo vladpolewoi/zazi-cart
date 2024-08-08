ac08c76aa6cca5cac4f5a96d77ce768c3459475f
vlad polewoi <vlad.polewoi1@gmail.com>
2024-08-08 14:10:53 +0300

    feat: :sparkles: Header

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,29 @@
 // import HelloWorld from './components/HelloWorld.vue'
 // import TheWelcome from './components/TheWelcome.vue'
 import Button from '@/components/ui/button/Button.vue'
+import ModeToggle from '@/components/ModeToggle.vue'
+import CartButton from '@/components/CartButton.vue'
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+  <header class="bg-slate-800 w-full sticky p-4 flex items-center justify-between top-0">
+    <div class="flex items-center gap-2">
+      <div class="bg-yellow-400 rounded-md p-1 w-10 h-10">
+        <img alt="Logo" src="./assets/logo.png" width="48" height="32" />
+      </div>
+      <span class="text-white font-bold text-xl">Zazi-Cart </span>
+    </div>
 
-    <div class="wrapper">
-      <!-- <HelloWorld msg="You did it!" /> -->
+    <div class="flex items-center justify-between gap-2">
+      <ModeToggle />
+      <CartButton :quantity="4" />
     </div>
   </header>
 
   <main>
+    <div class="w-full bg-yellow-200 h-dvh"></div>
+    <div class="w-full bg-yellow-200 h-dvh"></div>
+    <div class="w-full bg-yellow-200 h-dvh"></div>
     <p class="text-red-500">HELLO</p>
     <!-- <TheWelcome /> -->
     <Button>ShadCN</Button>
---
+++ src/components/CartButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="relative">
+    <Button variant="outline" size="icon">
+      <CartIcon class="fill-slate-800 h-6 w-6" />
+    </Button>
+    <span
+      v-if="quantity"
+      class="bg-yellow-500 text-white font-bold rounded-full text-xs h-5 w-5 -top-2 -right-2 absolute flex items-center justify-center"
+      >{{ quantity }}</span
+    >
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from '@/components/ui/button/Button.vue'
+import { CartIcon } from '@/assets/icons'
+
+interface Props {
+  quantity: number
+}
+
+defineProps<Props>()
+</script>

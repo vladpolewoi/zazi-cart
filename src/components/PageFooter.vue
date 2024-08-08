fdd7d84c420fdc35ea8dec0bfef767aabdda898e
vlad polewoi <vlad.polewoi1@gmail.com>
2024-08-08 14:16:34 +0300

    feat: :sparkles: Fotoer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="w-full bg-slate-800 text-slate-500 flex items-center justify-center h-20">
+    <span> @copyright text </span>
+  </footer>
+</template>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <header class="bg-slate-800 w-full sticky p-4 flex items-center justify-between top-0">
+    <div class="flex items-center gap-2">
+      <div class="bg-yellow-400 rounded-md p-1 w-10 h-10">
+        <img alt="Logo" src="./assets/logo.png" width="48" height="32" />
+      </div>
+      <span class="text-white font-bold text-xl">Zazi-Cart </span>
+    </div>
+
+    <div class="flex items-center justify-between gap-2">
+      <ModeToggle />
+      <CartButton :quantity="4" />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+import ModeToggle from '@/components/ModeToggle.vue'
+import CartButton from '@/components/CartButton.vue'
+</script>

0b1e0209637629f8eb2a7c324242ca14cb0b3521
vlad polewoi <vlad.polewoi1@gmail.com>
2024-08-09 16:49:24 +0300

    feat: :sparkles: Alert on delete + change quantity input

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,5 +1,9 @@
 import CartIcon from './CartIcon.vue'
+import DayIcon from './DayIcon.vue'
+import MinusIcon from './MinusIcon.vue'
+import NightIcon from './NightIcon.vue'
+import PlusIcon from './PlusIcon.vue'
 import StarIcon from './StarIcon.vue'
 import TrashIcon from './TrashIcon.vue'
 
-export { CartIcon, StarIcon, TrashIcon }
+export { CartIcon, DayIcon, MinusIcon, NightIcon, PlusIcon, StarIcon, TrashIcon }
--- src/components/CartProductCard.vue
+++ src/components/CartProductCard.vue
@@ -19,20 +19,48 @@
             <span class="inline-block mb-2 text-sm text-slate-400">Produced By: ShopName </span>
           </div>
           <div class="flex items-center gap-1">
-            <Button @click="onItemRemove" variant="outline" size="icon" class="group">
-              <TrashIcon class="fill-primary group-hover:fill-red-500 group w-4 h-4" />
-            </Button>
+            <AlertDialog :open="isAlertOpen">
+              <AlertDialogTrigger asChild>
+                <Button variant="outline" size="icon" class="group" @click="isAlertOpen = true">
+                  <TrashIcon class="fill-primary group-hover:fill-red-500 group w-4 h-4" />
+                </Button>
+              </AlertDialogTrigger>
+              <AlertDialogContent>
+                <AlertDialogHeader>
+                  <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                  <AlertDialogDescription>
+                    This action will remove product form your cart.
+                  </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                  <AlertDialogCancel @click="isAlertOpen = false">Cancel</AlertDialogCancel>
+                  <AlertDialogAction class="bg-red-500 hover:bg-red-600" @click="onItemRemove"
+                    >Remove</AlertDialogAction
+                  >
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialog>
           </div>
         </div>
       </CardContent>
 
       <CardFooter class="gap-2 justify-between">
-        <Input
+        <!-- <Input
           type="number"
           class="max-w-20"
-          :modelValue="toValue(quantity)"
-          @input="onQuantityChange"
-        />
+          min="1"
+          :modelValue="quantity.value"
+          @change="onQuantityChange"
+        /> -->
+        <div class="flex gap-4 items-center">
+          <Button size="icon" @click="onQuantityChange(-1)">
+            <MinusIcon class="fill-secondary h-5 w-5" />
+          </Button>
+          <span class="text-xl font-bold">{{ quantity }}</span>
+          <Button size="icon" @click="onQuantityChange(1)">
+            <PlusIcon class="fill-secondary h-5 w-5" />
+          </Button>
+        </div>
         <div class="flex flex-col">
           <span class="text-secondary-foreground">Total price:</span>
           <span class="text-2xl font-semibold">{{ totalPrice }}</span>
@@ -44,27 +72,45 @@
 
 <script setup lang="ts">
 import type { IProduct } from '@/api/product'
-import { StarIcon, TrashIcon } from '@/assets/icons'
+import { MinusIcon, PlusIcon, StarIcon, TrashIcon } from '@/assets/icons'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog'
 import Button from '@/components/ui/button/Button.vue'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import Input from '@/components/ui/input/Input.vue'
 import { formatCurrency } from '@/lib/utils'
 import { useCartStore } from '@/stores/cart'
-import { computed, toValue } from 'vue'
+import { computed, ref } from 'vue'
 
 interface Props {
   product: IProduct
 }
 
 const { product } = defineProps<Props>()
+
+const isAlertOpen = ref(false)
+
 const { removeItem, getQuantityById, updateQuantity } = useCartStore()
 const quantity = computed(() => getQuantityById(product.id))
 
 const totalPrice = computed(() => formatCurrency(product.price * quantity.value.value))
 
-function onQuantityChange(event: Event) {
-  const value = (event.target as HTMLInputElement).value
-  updateQuantity(product.id, Number(value))
+function onQuantityChange(value: number) {
+  const newQuantity = quantity.value.value + value
+
+  if (newQuantity < 1) {
+    isAlertOpen.value = true
+  } else {
+    updateQuantity(product.id, newQuantity)
+  }
 }
 
 function onItemRemove() {

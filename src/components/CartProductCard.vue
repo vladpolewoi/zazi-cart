f7ba8b4b7e82cc008a20ac54c11049a25064cae2
vlad polewoi <vlad.polewoi1@gmail.com>
2024-08-09 16:16:55 +0300

    feat: :sparkles: quantity

--- src/components/CartProductCard.vue
+++ src/components/CartProductCard.vue
@@ -13,10 +13,12 @@
           <div>
             <span class="text-xl font-bold">${{ product.price }}</span>
             <div class="text-sm">Stock: 12</div>
+            <div class="flex items-center gap-1">
+              <StarIcon class="fill-yellow-400" />{{ product.rating.rate }}
+            </div>
             <span class="inline-block mb-2 text-sm text-slate-400">Produced By: ShopName </span>
           </div>
           <div class="flex items-center gap-1">
-            <!-- <StarIcon class="fill-yellow-400" />{{ product.rating.rate }} -->
             <Button @click="onItemRemove" variant="outline" size="icon" class="group">
               <TrashIcon class="fill-primary group-hover:fill-red-500 group w-4 h-4" />
             </Button>
@@ -25,10 +27,15 @@
       </CardContent>
 
       <CardFooter class="gap-2 justify-between">
-        <Input type="number" class="max-w-20" />
+        <Input
+          type="number"
+          class="max-w-20"
+          :modelValue="toValue(quantity)"
+          @input="onQuantityChange"
+        />
         <div class="flex flex-col">
           <span class="text-secondary-foreground">Total price:</span>
-          <span class="text-2xl font-semibold">${{ product.price * 4 }}</span>
+          <span class="text-2xl font-semibold">{{ totalPrice }}</span>
         </div>
       </CardFooter>
     </Card>
@@ -36,23 +43,29 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-
-import { IProduct } from '@/api/product.ts'
-import { Card, CardTitle, CardHeader, CardFooter, CardContent } from '@/components/ui/card'
+import type { IProduct } from '@/api/product'
+import { StarIcon, TrashIcon } from '@/assets/icons'
 import Button from '@/components/ui/button/Button.vue'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import Input from '@/components/ui/input/Input.vue'
-import { TrashIcon } from '@/assets/icons'
-import { StarIcon } from '@/assets/icons'
+import { formatCurrency } from '@/lib/utils'
 import { useCartStore } from '@/stores/cart'
+import { computed, toValue } from 'vue'
 
 interface Props {
   product: IProduct
-  quantity: number
 }
 
-const { product, quantity } = defineProps<Props>()
-const { removeItem } = useCartStore()
+const { product } = defineProps<Props>()
+const { removeItem, getQuantityById, updateQuantity } = useCartStore()
+const quantity = computed(() => getQuantityById(product.id))
+
+const totalPrice = computed(() => formatCurrency(product.price * quantity.value.value))
+
+function onQuantityChange(event: Event) {
+  const value = (event.target as HTMLInputElement).value
+  updateQuantity(product.id, Number(value))
+}
 
 function onItemRemove() {
   removeItem(product.id)
--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -3,31 +3,45 @@ import { defineStore } from 'pinia'
 import { computed, ref } from 'vue'
 
 type Cart = {
-  quantity: number
-  product: IProduct
-}[]
+  [key: string]: {
+    quantity: number
+    product: IProduct
+  }
+}
 
 export const useCartStore = defineStore('cart', () => {
-  const cart = ref<Cart>([])
-  const totalItems = computed(() => cart.value.reduce((acc, item) => acc + item.quantity, 0))
+  const cart = ref<Cart>({})
+  const totalItems = computed(() =>
+    Object.values(cart.value).reduce((acc, item) => acc + item.quantity, 0)
+  )
+
+  const getQuantityById = (id: number) => computed(() => cart.value[id]?.quantity || 0)
 
   // const total = computed(() => cart.value.reduce((acc, item) => acc + item.product.price, 0))
 
-  const addItem = (item: IProduct, quantity: number) =>
-    cart.value.push({
+  const addItem = (item: IProduct, quantity: number) => {
+    cart.value[item.id] = {
       product: item,
-      quantity
-    })
+      quantity: (cart.value[item.id]?.quantity || 0) + quantity
+    }
+  }
 
   const removeItem = (id: number) => {
-    const index = cart.value.findIndex((item) => item.product.id === id)
-    cart.value.splice(index, 1)
+    const { [id]: _, ...rest } = cart.value
+
+    cart.value = rest
+  }
+
+  const updateQuantity = (id: number, quantity: number) => {
+    cart.value[id].quantity = quantity
   }
 
   return {
     cart,
     totalItems,
+    getQuantityById,
     addItem,
-    removeItem
+    removeItem,
+    updateQuantity
   }
 })
